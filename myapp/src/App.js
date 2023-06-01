a964a06336dd35a83eb5c977266d25324b14b6cb
carolina <carolinacapetilloc@gmail.com>
2023-06-01 14:54:47 -0700

    added work info and about me info. still needs styling

--- myapp/src/App.js
+++ myapp/src/App.js
@@ -17,8 +17,28 @@ function App() {
         </div>
       </div>
       <h1 class="name">Carolina Capetillo</h1>
+      <div class="basic-bio">
+        I'm a full stack engineer who enjoys making fun stuff for the web
+      </div>
       <div class="about-me">
-        I'm a full stack engineer who enjoys making fun stuff with software
+        <p>
+        Back in 2018, I received my Bachelor's degree in Psychology from University of California, Santa Barbara. A year later I decided to 
+        do a 180° and get into software. I finished General Assembly's Software Engineer Immersive bootcamp in February of 2020. There, I learned 
+        how to code with JavaScript, Python, and SQL, along with some databases, like MySQL and MongoDB, and computer science theory.
+        I'm now doing online school to learn how to code with Java and C++ 🤓, everyone's favorite languages!
+        </p>
+      </div>
+      <div class="about-work">
+        <p>
+        Outside of school, my primary focus these days is building products for clients and working on personal projects. But most importantly
+        I am actively looking for work. HIRE ME!
+        </p>
+      </div>
+      <div class="personal">
+        <p>
+        When I'm not creating apps or applying to jobs, I am mostly rock climbing, training for a half marathon, walking my dogs,
+        or baking. [insert photos of me baking]
+        </p>
       </div>
       </div>
 
