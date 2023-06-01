dcd0a5b05b5a0df3e5e0f3c1a9d87ceaf3c2f270
carolina <carolinacapetilloc@gmail.com>
2023-06-01 13:18:46 -0700

    added very basic nav bar. no styling

--- myapp/src/App.js
+++ myapp/src/App.js
@@ -1,24 +1,23 @@
-import logo from './logo.svg';
 import './App.css';
+import image from './favicon.png'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div class="container">
+        {/* <img src={image} alt="Rolling Circle" class="rolling-circle"/> */}
+      </div>
+      <div id="menu-outer">
+        <div class="table">
+          <ul id="horizontal-list">
+            <li>About</li>
+            <li>Projects</li>
+            <li>Experience</li>
+          </ul>
+        </div>
+      </div>
+      </div>
+
   );
 }
 
